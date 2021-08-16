8fbb31c35bb8b5a8311e32ec09411d8412cf66cf
bryan <thenhernandezb@gmail.com>
2021-08-16 09:13:51 -0400

    add: render users

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllUser } from "./service/serviceUser";
 
-const App=()=> {
+const App = () => {
   return (
     <div className="App">
       <HomePage />
     </div>
   );
-}
+};
 
 const HomePage = () => {
-  const allUser = getAllUser()
-  
+  const [userd, setUserd] = useState([{ _name: "" }]);
+
+  useEffect(() => {
+    getAllUser().then((response) => {
+      setUserd(response);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Club Acuatico Los Fenix</h1>
       <p>Somos un equipo comprometido con el crecimiento deportivo</p>
+      <ul>
+        {userd.map((user) => {
+          return (
+            <li key={user._name}>
+              <p>{user._name}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
-
-
+};
 
 export default App;
